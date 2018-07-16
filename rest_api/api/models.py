df55a0f00b9ae1b9674caebe611d3144b6c7504e
kayne10 <troy.kayne@colorado.edu>
2018-07-16 13:46:52 -0600

    first commit, added custom user model, manager, and migrations to db

---
+++ rest_api/api/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+# Create your models here.
+class UserProfileManager(BaseUserManager):
+    """Helps Django work with the custom user model"""
+
+    def create_user(self, email, name, password):
+        """Creates a new user profile"""
+
+        if not email:
+            raise ValueError('Users must have an email address.')
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Creates and saves a new superuser"""
+
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Represents the custom user model inside the system"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        """Human readable interpretation of our UserProfile model"""
+        return self.email + " - " + self.get_full_name()
